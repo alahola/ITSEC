1bc82483c257fede31d210346c2c2a41ebb99449
alahola <a.ahola@tu-bs.de>
2017-11-10 11:59:52 +0100

    get possible

--- Uebung2AL/sectubs/crack_dh.py
+++ Uebung2AL/sectubs/crack_dh.py
@@ -4,9 +4,27 @@ Created on 10.11.2017
 @author: Anna-Liisa
 '''
 import argparse
+from sectubs.crack_dh import getPossibleSecretAlice
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-g', type=int)
 parser.add_argument('-n', type=int)
 parser.add_argument('--alice', type=int)
 parser.add_argument('--bob', type=int)
+
+
+generator = parser.g;
+prime = parser.n;
+
+getPossibleSecretAlice(6, 3, 17);
+
+def getPossibleSecretAlice(alicePublic, generator, prime):
+    i =0;
+    possibleSecretAlice = []
+    while i < 200:
+        if(generator^i % prime == alicePublic):
+            possibleSecretAlice.append(i)
+        i += 1
+            
+    
+    print(possibleSecretAlice) 
