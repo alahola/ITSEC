f10f3fc4e73a005242e4eb2b32de153df8d9f0f9
Leif Scheppelmann <fiel@live.de>
2017-12-11 20:58:49 +0100

    unittest is wokring

--- Uebung5/src/send_packet.py
+++ Uebung5/src/send_packet.py
@@ -1,112 +1,113 @@
-# some imports
 import argparse
-import socket, sys
-from struct import *
+import socket
+from struct import pack
 
-# checksum functions needed for calculation checksum
-def checksum(msg):
+
+def tcp_checksum(msg):
     s = 0
-
     # loop taking 2 characters at a time
     for i in range(0, len(msg), 2):
-        w = ord(msg[i]) + (ord(msg[i+1]) << 8 )
+        w = msg[i] + (msg[i + 1] << 8)
         s = s + w
-
-        s = (s>>16) + (s & 0xffff)
+    s = (s >> 16) + (s & 0xffff)
     s = s + (s >> 16)
-
-    #complement and mask to 4 byte short
     s = ~s & 0xffff
-
     return s
+
 
 parser = argparse.ArgumentParser(description='send packet')
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('--syn')
-group.add_argument('--xmas')
-group.add_argument('--fin')
-group.add_argument('--null')
+group.add_argument('--syn', action='store_true')
+group.add_argument('--xmas', action='store_true')
+group.add_argument('--fin', action='store_true')
+group.add_argument('--null', action='store_true')
 parser.add_argument('DOMAIN', type=str)
 parser.add_argument('PORT', type=int)
 args = parser.parse_args()
 
-#create a raw socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
-except socket.error as msg:
-    print('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
+# TCP packet
+# 2byte
+src_port = 1234
+dest_port = args.PORT
+# 4byte
+seq_num = 0
+# 4byte
+ack_num = 0
+# 4it
+data_off = (5 << 4) + 0
+# 1bit
+cwr_flag = False
+ece_flag = False
+urg_flag = args.xmas
+ack_flag = False
+psh_flag = args.xmas
+rst_flag = False
+syn_flag = args.syn
+fin_flag = True if args.fin | args.xmas else False
 
-# tell kernel not to put in headers, since we are providing it, when using IPPROTO_RAW this is not necessary
-# s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+tcp_flags = \
+    fin_flag \
+    + (syn_flag << 1) \
+    + (rst_flag << 2) \
+    + (psh_flag << 3) \
+    + (ack_flag << 4) \
+    + (urg_flag << 5) \
+    + (ece_flag << 6) \
+    + (cwr_flag << 7)
 
-# now start constructing the packet
-packet = '';
+# 2byte
+win = socket.htons(5840)
+checksum = 0
+urgent_pointer = 0
+# 4byte
+options = 0
 
-source_ip = '192.168.1.101'
-dest_ip = socket.gethostbyname(args.DOMAIN)# or socket.gethostbyname('www.google.com')
+tcp_packet = pack(
+    '!HHLLBBHHH',
+    src_port,
+    dest_port,
+    seq_num,
+    ack_num,
+    data_off,
+    tcp_flags,
+    win,
+    checksum,
+    urgent_pointer
+)
 
-# ip header fields
-ip_ihl = 5
-ip_ver = 4
-ip_tos = 0
-ip_tot_len = 0  # kernel will fill the correct total length
-ip_id = 54321   #Id of this packet
-ip_frag_off = 0
-ip_ttl = 255
-ip_proto = socket.IPPROTO_TCP
-ip_check = 0    # kernel will fill the correct checksum
-ip_saddr = socket.inet_aton ( source_ip )   #Spoof the source ip address if you want to
-ip_daddr = socket.inet_aton ( dest_ip )
+# TCP-Pseudoheader
+# 4byte
+src_ip = b'127.0.0.1'
+dest_ip = bytes(socket.gethostbyname(args.DOMAIN), 'UTF-8')
+# 2byte
+prot = socket.IPPROTO_TCP
+tcp_len = len(tcp_packet)
 
-ip_ihl_ver = (ip_ver << 4) + ip_ihl
+pseudo_header = pack(
+    '!4s4sBBH',
+    src_ip,
+    dest_ip,
+    0,
+    prot,
+    tcp_len
+)
 
-# the ! in the pack format string means network order
-ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+checksum = tcp_checksum(pseudo_header + tcp_packet)
 
-# tcp header fields
-tcp_source = 1234   # source port
-tcp_dest = args.PORT   # destination port
-tcp_seq = 454
-tcp_ack_seq = 0
-tcp_doff = 5    #4 bit field, size of tcp header, 5 * 4 = 20 bytes
-#tcp flags
-tcp_fin = 1 if args.xmas is not None | args.fin is not None else 0
-tcp_syn = 1 if args.syn is not None else 0
-tcp_rst = 0
-tcp_psh = 1 if args.xmas is not None else 0
-tcp_ack = 0
-tcp_urg = 1 if args.xmas is not None else 0
-tcp_window = socket.htons (5840)    #   maximum allowed window size
-tcp_check = 0
-tcp_urg_ptr = 0
+tcp_packet = pack(
+    '!HHLLBBH',
+    src_port,
+    dest_port,
+    seq_num,
+    ack_num,
+    data_off,
+    tcp_flags,
+    win
+) + pack(
+    'H', checksum
+) + pack(
+    '!H', urgent_pointer
+)
 
-tcp_offset_res = (tcp_doff << 4) + 0
-tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
-
-# the ! in the pack format string means network order
-tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_check, tcp_urg_ptr)
-
-user_data = 'Hello, how are you'
-
-# pseudo header fields
-source_address = socket.inet_aton(source_ip)
-dest_address = socket.inet_aton(dest_ip)
-placeholder = 0
-protocol = socket.IPPROTO_TCP
-tcp_length = len(tcp_header) + len(user_data)
-
-psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
-psh = psh + tcp_header + user_data;
-
-tcp_check = checksum(psh)
-#print tcp_checksum
-
-# make the tcp header again and fill the correct checksum - remember checksum is NOT in network byte order
-tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
-
-# final full packet - syn packets dont have any data
-packet = ip_header + tcp_header + user_data
-
-#Send the packet finally - the port specified has no effect
-s.sendto(packet, (dest_ip , 0 )) # put this in a loop if you want to flood the target
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+s.sendto(tcp_packet, (socket.gethostbyname(args.DOMAIN), 0))
