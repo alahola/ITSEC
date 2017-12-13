8b52327967ff44c019e1833d1865e11394032c50
alahola <a.ahola@tu-bs.de>
2017-12-13 13:32:24 +0100

    Es Funkt

--- Uebung5/src/port_knocker/client.py
+++ Uebung5/src/port_knocker/client.py
@@ -1,29 +1,109 @@
 import argparse
 from _sha256 import sha256
 
-from scapy.layers.inet import IP, UDP
+from scapy.layers.inet import IP, UDP, send, sniff, TCP
 from scapy.sendrecv import sr1
+from scapy.layers.dns import DNS
+import random
 
 
 class client(object):
 
-    def __init__(self, host, port, key, knocks):
+    def __init__(self, host, port, key: int, knocks):
         self.key = key
         self.knocks = knocks
         self.host = host
         self.port = port
-
-        packet = IP(
-            dst=host
+        
+        c_packet = IP(
+            dst='localhost'
         ) / UDP(
-            dport=port
+            dport=6666
         )
-        ans = sr1(packet)
+        send(c_packet)
+        
+        while True:
+            packets = sniff(count=1, filter='udp and port ' + str(port))
+            print(packets)
+            packet = packets[0]
+            
+            self.getLayersOfPacket(packet)
+            
+            if packet and packet.haslayer(UDP):
+                udp = packet["UDP"]
+                challenge = udp.payload
+                print("Client received following challenge: " , challenge)
+                
+                knock_point = 0;
+                
+                ports = [] 
+                
+                c = self.byteToString(challenge)
+                cInt = int(c)
+                while knock_point < self.knocks:
+                    
+                    ports.append(self.p(knock_point, cInt, key))
+                    knock_point = knock_point+1
+                
+                for port in ports:
+                    tcp_packet = IP(
+                    dst='localhost'
+                    ) / TCP(
+                    sport=port,
+                    dport=port
+                    )
+                    
+                    send(tcp_packet) 
+                
+            
 
 
+            
+            
+            
+           
+#             if packet and packet.haslayer(UDP):
+#                 print(packet)
 
-    def p(self, i: int, c: int):
-        return 1024 + (sha256(self.key * c + i) % 28657)
+#         packet = IP(
+#             dst=host
+#         ) / UDP(
+#             dport=port
+#         )
+#         ans = sr1(packet)
+
+    def getLayersOfPacket(self, packet):
+        layers = []
+        counter = 0
+        
+        while packet.getlayer(counter) != None:
+            layer = packet.getlayer(counter)
+            if (layer != None):
+                print(layer.name)
+                layers.append(layer.name)
+            else:
+                break
+            counter += 1
+
+
+        udp = packet["UDP"]
+        print(udp.payload)
+        print("Layers are:\t\t",layers)
+    
+            
+    def byteToString(self, byte: bytes):
+        string = str(byte)
+        return string[2:len(string)-1] 
+
+    def p(self, i: int, c: int, k: int):
+        myString = str((k * c + i))
+        print(myString)
+        encodedString = myString.encode()
+        sharesult = sha256(encodedString)
+        val_hex = sharesult.hexdigest()
+        val_int = int(val_hex, 16)
+        return 1024 + (val_int % 28657)
+
 
 
 
@@ -33,3 +113,5 @@ parser.add_argument('--num-knocks', metavar='INT', type=int, required=True)
 parser.add_argument('host', metavar='IP/DOMAIN', type=str)
 parser.add_argument('port', metavar='PORT', type=int)
 args = parser.parse_args()
+
+s = client(args.host, args.port, args.shared_key, args.num_knocks)
--- Uebung5/src/port_knocker/server.py
+++ Uebung5/src/port_knocker/server.py
@@ -3,52 +3,116 @@ import random
 from _sha256 import sha256
 from _thread import start_new_thread
 
-from scapy.layers.inet import IP, UDP
-from scapy.sendrecv import sniff, send, sendp
+from scapy.layers.inet import IP, UDP, Raw, TCP
+from scapy.sendrecv import sniff, send, sendp, sr1
 
 
 class server(object):
 
-    def __init__(self, port, key, knocks):
+    def __init__(self, port, key : int, knocks):
         self.port = port
         self.key = key
         self.knocks = knocks
         while True:
+            
             packets = sniff(count=1, filter='udp and port ' + str(port))
+            print(packets)
             packet = packets[0]
-            c = random.randint(0, 100)
-            c_packet = IP(
-                dst=packet['IP'].src,
-                src=packet['IP'].dst
-            ) / UDP(
-                sport=port,
-                dport=packet['UDP'].sport
-            ) / bytes(c)
-            sendp(c_packet)
-            ports = []
-            for i in range(0, knocks - 1):
-                ports.append(self.p(i, c))
-            start_new_thread(self.observeKnocks(packet['IP'].src), ports)
+            if packet and packet.haslayer(UDP):
+                print("Is UDP")
+                c = random.randint(0, 100)
+                print("this ist random c : ", c)
+                c_packet = IP(
+                    dst=packet['IP'].src,
+                    src=packet['IP'].dst
+                ) / UDP(
+                    sport=port,
+                    dport=packet['UDP'].sport
+                ) / str(c)
+                send(c_packet)
+                ports = []
+                
+                knock_point = 0;
+                
+                
+                
+                while knock_point < self.knocks:
+                    
+                    ports.append(self.p(knock_point, c, key))
+                    knock_point = knock_point+1
+                
+                
+                while True:
+                    packets = sniff(count=self.knocks, filter='udp and port')
+#                     packets = sniff(count=1, filter='udp and port ' + str(port))
+                    print(packets)
+                    if len(packets) == self.knocks:
+                        knock_point = 0
+                        isValidKnock = False
+                        for packet in packets:
+                            if packet and packet.haslayer(TCP):
+                                tcp = packet["TCP"]
+                                dport = tcp.dport
+                                print("Current port" , dport)
+                                
+                                print("Current port in list", ports[knock_point])
+                                if(dport == ports[knock_point]):
+                                    isValidKnock = True
+                                else:
+                                    isValidKnock = False
+                                    break
+                                knock_point = knock_point+1
+                                
+                        
+                        if isValidKnock == True:
+                            print("unlocked")
+                            
+                            
+                            
+                        break;
+                    
+            break;
+                        
+                                
+                            
+                            
+                            
+                            
+                            
+                        
+                    
+                    
+                
+                
+#                 for i in range(0, knocks):
+#                     ports.append(self.p(i, c))
+#                 start_new_thread(self.observeKnocks(packet['IP'].src , ports))
 
     def observeKnocks(self, src: str, ports: list):
         print("Starting challenge with " + src + " on ports " + str(ports))
         filter = 'tcp and tcp[tcpflags] & (tcp-syn) != 0 and ip host ' + src
-        if len(list) != 1:
+        if len(ports) != 1:
             filter += ' and dst port ('
             first = True
-            for port in list:
+            for p in ports:
                 if first:
-                    filter += port
+                    filter += str(p)
                 else:
-                    filter += ' or ' + port
+                    filter += ' or ' + str(p)
             filter += ')'
         else:
-            filter += ' and dst port ' + list[0]
+            filter += ' and dst port ' + str(ports[0])
         packets = sniff(count=self.knocks, filter=filter)
         print('unlocked')
 
-    def p(self, i: int, c: int):
-        return 1024 + (sha256(self.key * c + i) % 28657)
+    def p(self, i: int, c: int, k: int):
+        myString = str((k * c + i))
+        print(myString)
+        encodedString = myString.encode()
+        sharesult = sha256(encodedString)
+        val_hex = sharesult.hexdigest()
+        val_int = int(val_hex, 16)
+        return 1024 + (val_int % 28657)
 
 
 parser = argparse.ArgumentParser(description='monitor')
@@ -58,3 +122,4 @@ parser.add_argument('--num-knocks', metavar='INT', type=int, required=True)
 args = parser.parse_args()
 
 s = server(args.port, args.shared_key, args.num_knocks)
+  
